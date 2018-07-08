835312c52802b5f7f21cea8f30a61a126122cae8
Rodrigo Dantas <rodrigodc07@gmail.com>
2018-07-08 16:59:14 -0300

    Adiciona cadastro de lojas, sellers e produtos

--- controller.py
+++ controller.py
@@ -1,18 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify ,render_template
 import requests
 
 app = Flask(__name__)
 
-URL = 'http://localhost:8080/api/sellers/'
+URL = 'http://localhost:8080/api/'
 
 
-@app.route('/')
-def root():
-    # sending get request and saving the response as response object
-    r = requests.get(url=URL)
+@app.route('/sellers')
+def sellers():
+    seller_URL = URL + 'sellers'
+    r = requests.get(url=seller_URL)
     # extracting data in json format
-    return jsonify(r.json())
+    aux = jsonify(r.json())
+    return render_template('register_seller.html')
 
+
+@app.route('/products')
+def products():
+    products_URL = URL + 'products'
+    r = requests.get(url=products_URL)
+    # extracting data in json format
+    aux = jsonify(r.json())
+    return render_template('register_product.html')
+
+
+@app.route('/stores')
+def stores():
+    stores_URL = URL + 'stores'
+    r = requests.get(url=stores_URL)
+    # extracting data in json format
+    aux = jsonify(r.json())
+    return render_template('register_store.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
